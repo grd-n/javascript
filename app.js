f3776de10a8985a6bb25d12c9ce1900203fa42c5
grdn <cjdgkr9907@yonsei.ac.kr>
2022-02-18 00:00:34 +0900

    자바스크립트 연습장1

---
+++ app.js
@@ -0,0 +1,57 @@
+const a = 3;
+const b = 4;
+
+let myName = "JW";
+
+console.log(myName);
+
+console.log(a+b);
+console.log(a*b);
+console.log(a/b);
+
+myName = "Hi"
+console.log(myName);
+
+const amIFat = false;
+console.log(amIFat);
+console.log(typeof(amIFat));
+
+const c = undefined;
+let d;
+console.log(d);
+
+const mon = "mon";
+const tue = "tue";
+const wed = "wed";
+const thu = "thu";
+const fri = "fri";
+const sat = "sat";
+const sun = "sun";
+
+const daysOfweek = mon+tue+wed+thu+fri+sat+sun;
+console.log(daysOfweek);
+
+const daysOfweek2 = [mon, tue, wed, thu, fri, sat, sun];
+
+nonsense = [1,2,"hello",false,null,true,undefined,"nico"];
+
+console.log(daysOfweek2);
+console.log(nonsense);
+
+console.log(daysOfweek2[5]);
+
+const h = 4;
+const i = 5;
+const j = 8;
+
+const arr = [a,b,c,[h,i]];
+console.log(arr[3]);
+
+arr.push(j);
+
+console.log(arr);
+
+const toBuy = ["a","b","c","d","e"];
+toBuy.push("f");
+
+console.log(toBuy);
